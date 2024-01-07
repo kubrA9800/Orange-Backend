9d6ecdb2f36ae4a7fb07d76c8b55edc0d13abdec
Mamedova Kubra <kiubrajm@code.edu.az>
2024-01-07 19:14:44 +0400

    added home page

--- Orange-Backend/obj/Debug/net8.0/Orange-Backend.AssemblyInfo.cs
+++ Orange-Backend/obj/Debug/net8.0/Orange-Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orange-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac75b220f4f898d7dfdb06746a77d6859c963c5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orange-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orange-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
